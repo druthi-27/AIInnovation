d8e41a4f8ba88acd83f8b2fe9a3503351fe2e046
Druthi Ramesh <druthi.ramesh@alteryx.com>
2025-06-13 12:39:26 +0530

    Added icon in navbar

--- src/App.jsx
+++ src/App.jsx
@@ -80,6 +80,27 @@ export default function App() {
         sx={{ bgcolor: "#23272f", boxShadow: 0, zIndex: (theme) => theme.zIndex.drawer + 1 }}
       >
         <Toolbar variant="dense">
+          <Box  sx={{
+            display: "flex",
+            alignItems: "center",
+            mr: 2,
+            gap: 2,
+            height: 48,
+          }}>
+          <span class="dots-icon">
+            <svg width="28" height="30" viewBox="0 0 24 24" fill="#B0B3B8" xmlns="http://www.w3.org/2000/svg">
+              <circle cx="6" cy="6" r="2"/>
+              <circle cx="12" cy="6" r="2"/>
+              <circle cx="18" cy="6" r="2"/>
+              <circle cx="6" cy="12" r="2"/>
+              <circle cx="12" cy="12" r="2"/>
+              <circle cx="18" cy="12" r="2"/>
+              <circle cx="6" cy="18" r="2"/>
+              <circle cx="12" cy="18" r="2"/>
+              <circle cx="18" cy="18" r="2"/>
+            </svg>
+          </span>
+          </Box>
           <Box
             sx={{
               display: "flex",
